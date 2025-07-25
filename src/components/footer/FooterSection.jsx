7c96edd7afe224bc526bc1af6e7844ba5efe49e7
rootbixinfotech <info@rastainfotech.com>
2025-07-25 15:22:20 +0530

    07/25/2025- 3:22

--- src/components/footer/FooterSection.jsx
+++ src/components/footer/FooterSection.jsx
@@ -31,7 +31,7 @@ const FooterSection = () => {
   const navigate = useNavigate();
 
   return (
- <footer className="bg-[#0B121B] text-white py-12 px-6 md:px-16">
+    <footer className="bg-[#0B121B] text-white py-12 px-6 md:px-16">
       {/* Grid Section */}
       <div className="max-w-7xl mx-auto grid md:grid-cols-3 gap-10 md:gap-20 lg:gap-28">
         {/* Logo + Description + Social Icons */}
@@ -41,13 +41,17 @@ const FooterSection = () => {
               <img src={logo_white} alt="logo_white" className="h-10 w-auto" />
             </Link>
             <Link to="/" className="flex items-center">
-              <img src={rastainfotech_white} alt="rasta_infotech" className="h-5 w-auto" />
+              <img
+                src={rastainfotech_white}
+                alt="rasta_infotech"
+                className="h-5 w-auto"
+              />
             </Link>
           </div>
           <p className="text-sm text-gray-300 text-justify">
-            Your trusted partner on the Road to Success! We provide end-to-end IT solutions,
-            from SAP and digital marketing to AI, cybersecurity, and beyond, fueling innovation
-            and growth for your business.
+            Your trusted partner on the Road to Success! We provide end-to-end
+            IT solutions, from SAP and digital marketing to AI, cybersecurity,
+            and beyond, fueling innovation and growth for your business.
           </p>
           <div className="flex gap-4 text-xl mt-4">
             <FaFacebookF />
@@ -74,7 +78,9 @@ const FooterSection = () => {
 
         {/* Useful Links */}
         <div>
-          <h3 className="text-lg font-semibold mb-4 text-white">Useful Links</h3>
+          <h3 className="text-lg font-semibold mb-4 text-white">
+            Useful Links
+          </h3>
           {links.map((item, idx) => (
             <Link
               to={item.path}
@@ -92,7 +98,8 @@ const FooterSection = () => {
           <div className="flex items-start gap-2 text-gray-300 text-sm mb-3">
             <HiOutlineLocationMarker className="mt-1 text-justify " />
             <span>
-              Office Address: 23 2nd Cross, Near Malnad, Coaching Centr MHR Layout, Bangalore North, Karnataka, India, 560090
+              Office Address: 23 2nd Cross, Near Malnad, Coaching Centr MHR
+              Layout, Bangalore North, Karnataka, India, 560090
             </span>
           </div>
           <div className="flex items-center gap-2 mb-2 text-gray-300">
@@ -107,7 +114,8 @@ const FooterSection = () => {
       {/* Bottom Section */}
       <div className="mt-10 border-t border-gray-700 pt-6 text-sm text-gray-400 flex flex-col md:flex-row justify-between items-center gap-4">
         <p>
-          Copyright 2025, <span className="text-green-500">Rasta Infotech</span>. All Rights Reserved.
+          Copyright 2025, <span className="text-green-500">Rasta Infotech</span>
+          . All Rights Reserved.
         </p>
       </div>
 
@@ -137,12 +145,17 @@ const FooterSection = () => {
         >
           Cookies Setting
         </span>
+        <a
+          href="https://www.rastainfotech.com/sitemap.xml"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer hover:text-white"
+        >
+          Site Map
+        </a>
       </div>
     </footer>
   );
 };
-
-
-
 
 export default FooterSection;
